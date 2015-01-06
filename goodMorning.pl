4488cb3fb274bfb684ea6e3eeb4b16b80f886834
Eric Hopkins <scarecrow3322@gmail.com>
2015-01-06 00:50:45 -0500

    add ability to specify branch, and a help flag

--- goodMorning.pl
+++ goodMorning.pl
@@ -10,6 +10,9 @@
 # This script is designed to run through a root directory of projects and do
 # pulls on each of them.
 #
+# If you would like to make sure the pull is done to a specific branch,
+# provide a second argument with the name of that branch (i.e. staging)
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
@@ -26,7 +29,9 @@
 use strict;
 use warnings;
 
-die "Usage: 1 Argument required. Provide the root directory holding the Git repositories you wish to have updated." unless (@ARGV == 1);
+die "Usage:\nARG 1: root directory of git repositories.\nARG 2 (optional): Name of the branch you would like the repositories set to.\n" if ($ARGV[0] eq '-h');
+
+die "Usage: 1 Argument required. Provide the root directory holding the Git repositories you wish to have updated.\nFor help, please type -h.\n" unless (@ARGV >= 1);
 
 my $root;
 
@@ -45,5 +50,6 @@ foreach my $p (@projects) {
 
     print "\n\ncurrently in: $p\n";
     chdir($p);
+	system("git checkout $ARGV[1]") if @ARGV == 2;
     system("git pull");
 }
