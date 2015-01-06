b4862e45ea60e724c93c735c15b15651fb6e128b
Eric Hopkins <scarecrow3322@gmail.com>
2015-01-06 16:58:21 -0500

    fix issues with missing quote marks

--- gitAliases.sh
+++ gitAliases.sh
@@ -21,13 +21,13 @@
 # with this program. If not, see <http://www.gnu.org/licenses/>.
 ###############################################################################
 
-echo `git config --global alias.aliases config --get-regex alias`
+echo `git config --global alias.aliases 'config --get-regex alias'`
 echo `git config --global alias.ci 'commit -m'`
 echo `git config --global alias.co checkout`
 echo `git config --global alias.cob 'checkout -b'`
 echo `git config --global alias.s 'status -s'`
-echo `git config --global alias.whoami config user.email`
-echo `git config --global alias.beyonce config user.name`
+echo `git config --global alias.whoami 'config user.email'`
+echo `git config --global alias.beyonce 'config user.name'`
 echo `git config --global alias.hist 'log --oneline'`
 echo `git config --global alias.untrack 'rm --cache'`
 echo `git config --global alias.all 'add .'`
