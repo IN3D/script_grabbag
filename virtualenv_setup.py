0c18a11f45124283a718f024737947f2976d928b
Eric Hopkins <scarecrow3322@gmail.com>
2015-01-08 14:13:17 -0500

    insert the header for virtual environment script

---
+++ virtualenv_setup.py
@@ -0,0 +1,22 @@
+###############################################################################
+# Author: Eric Hopkins
+# Date: 01/08/15
+#
+# This script is designed to automate the setup of a python virtual
+# environment. The script takes in an argument for the version of python that
+# you would like to virtualize in your environment. If no argument is provided,
+# python3 will be assumed.
+#
+# This program is free software: you can redistribute iit and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+###############################################################################
